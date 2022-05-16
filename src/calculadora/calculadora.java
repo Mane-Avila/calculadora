4d8c3e89142e7a3f55ef1c88695eedcd1b366920
Manuel Avila <manuel.avila-fernandez@iesruizgijon.com>
2022-05-16 10:30:58 +0200

    Creada clase calculadora con atributos y constructores

---
+++ src/calculadora/calculadora.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Mane
+ */
+public class calculadora {
+     private double numero1;
+    private double numero2;
+// clase con constructor pasando dos numeros reales, setters , getters y métodos para devolver el resultado de sumar, restar , multiplicar y dividir
+
+    public calculadora(double numero1, double numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+    
+
+    public calculadora(){
+        
+    }
+}
